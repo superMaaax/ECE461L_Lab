ffc9239b354be01f5a6a7aa332bc37901149b054
swadeeptoc <swadeepto@utexas.edu>
2024-10-14 15:49:03 -0500

    updated flask app

--- thurs_night/app.py
+++ thurs_night/app.py
@@ -134,4 +134,5 @@ def checkin():
 
 if __name__ == "__main__":
     initialize_hardware()
-    app.run(debug=True, port=5000)
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port)
